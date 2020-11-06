85cd08f1c9b2e4ff7d0bbd01e04105daa215e484
gbenedisgrab <gbenedisgrab@packer.edu>
2020-11-06 12:58:38 -0500

    I am trying to simulate a comflict to see what happens.  I changed a print line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print('this is prior')
 print('hello world')
-print('goodbye world')
+print('goodbye world complicated changes')
 for i in range(10):
 	print(i,end=' ')
 print('this is the last line for now')
