5b28d7aa3352a6df097cac37e133d3910bf1d639
Greg Benedis-Grab <gbenedisgrab@packer.edu>
2020-11-12 19:26:48 -0500

    Made file output version of the coins problem

--- coins.py
+++ coins.py
@@ -1,6 +1,5 @@
-coins = [1,2,5,10,20,50,100,200]
+coins = [1,2,5,10,20,50,100,200,500]
 goal = 200
-
 
 def coins_solutions(goal):
     solutions = [1] + ([0]*goal)
@@ -13,19 +12,25 @@ def get_val(amounts):
     return sum([coins[i]*amounts[i] for i in range(len(coins))])
 
 def next_combo(combo):
-    i = 0
-    pass
-    for i in range(len(combo)):
-        combo[i]+=1
+    c = combo[:]
+    for i in range(len(c)):
+        c[i]+=1
+        if get_val(c) > goal:
+            c[i] = 0
+        else:
+            return c
+    return False
     
-def coin_combinations(goal):
-    solutions = []
+def coin_combos(goal):
     amounts = [0]*len(coins)
-    
+    while amounts:
+        if get_val(amounts) == goal:
+            yield amounts
+        amounts = next_combo(amounts)
 
+print(coins_solutions(goal))
 
-
-print(coins_solutions(coins,goal))
-coin_combinations(coins,goal)
-print(get_val(coins,[1]*len(coins)))
+with open('combos.txt','w') as f:
+    for combo in coin_combos(goal):
+        f.write(','.join(map(str,combo))+'\n')
 
