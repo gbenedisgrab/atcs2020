171f063d3d45df1c5ca9e2c32510f5b2c9aff9ed
Greg Benedis-Grab <gbenedisgrab@packer.edu>
2020-11-15 22:54:32 -0500

    Got 3 approaches to the problem now

--- coins.py
+++ coins.py
@@ -1,7 +1,8 @@
-coins = [1,2,5,10,20,50,100,200,500]
-goal = 200
+import time
 
-def coins_solutions(goal):
+coins = [1,2,5,10,20,50,100,200]
+
+def count_coin_solutions(goal):
     solutions = [1] + ([0]*goal)
     for coin in coins:
         for j in range(coin, goal+1):
@@ -11,7 +12,7 @@ def coins_solutions(goal):
 def get_val(amounts):
     return sum([coins[i]*amounts[i] for i in range(len(coins))])
 
-def next_combo(combo):
+def next_combo(combo,goal):
     c = combo[:]
     for i in range(len(c)):
         c[i]+=1
@@ -26,11 +27,32 @@ def coin_combos(goal):
     while amounts:
         if get_val(amounts) == goal:
             yield amounts
-        amounts = next_combo(amounts)
+        amounts = next_combo(amounts,goal)
 
-print(coins_solutions(goal))
+def build_solutions(goal):
+    solutions = [[[]]] + [[] for _ in range(goal)]
+    for coin in coins:
+        for j in range(coin, goal+1):
+            solutions[j].extend([combo+[coin] for combo in solutions[j-coin]])
+    return solutions[goal]
 
-with open('combos.txt','w') as f:
-    for combo in coin_combos(goal):
-        f.write(','.join(map(str,combo))+'\n')
+def writeit(solutions, file_name):
+    with open(file_name,'w') as f:
+        for combo in solutions:
+            f.write(','.join(map(str,combo))+'\n')
 
+def main():
+    value = 150
+
+    start = time.time()
+    print(count_coin_solutions(value),'time',time.time()-start)
+
+    start = time.time()
+    writeit(coin_combos(value),'combos1.txt')
+    print('finished first method','time',time.time()-start)
+
+    start = time.time()
+    writeit(build_solutions(value),'combos2.txt')
+    print('finished second method','time',time.time()-start)
+
+main()
